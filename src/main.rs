86daf2a6b414d56fc4fba5cd2545841e82f59922
VvyLw <owl1465fu9@gmail.com>
2024-10-11 22:05:10 +0900

    macros deleted

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,5 @@
-#![allow(unused_macros)]
-
+#[allow(unused_imports)]
 use itertools::Itertools;
-
-macro_rules! rep {
-    ($n: expr, $body: block) => {
-        for _ in 0..$n {
-            $body
-        }
-    };
-    ($i: ident, $n: expr, $body: block) => {
-        for $i in 0..$n {
-            $body
-        }
-    };
-    ($i: ident, $a: expr, $b: expr, $body: block) => {
-        for $i in $a..=$b {
-            $body
-        }
-    };
-    ($i: ident, $a: expr, $b: expr, $c: expr, $body: block) => {
-        let mut $i = $a;
-        while $i <= $b {
-            $body
-            $i += $c;
-        }
-    };
-}
-
-macro_rules! rvp {
-    ($i: ident, $n: expr, $body: block) => {
-        for $i in (0..$n).rev() {
-            $body
-        }
-    };
-    ($i: ident, $a: expr, $b: expr, $body: block) => {
-        for $i in ($b..=$a).rev() {
-            $body
-        }
-    };
-    ($i: ident, $a: expr, $b: expr, $c: expr, $body: block) => {
-        let mut $i = $a;
-        while $i >= $b {
-            $body
-            $i -= $c;
-        }
-    };
-}
-
-macro_rules! each {
-    ($el: ident, $v: expr, $body: block) => {
-        for $el in $v {
-            $body
-        }
-    }
-}
-
-#[allow(dead_code)]
-fn out<T: std::fmt::Display>(x: T) {
-    print!("{}", x);
-}
-macro_rules! out {
-    () => {
-        println!();
-    };
-    ($head:expr $(, $tail:expr)*) => {
-        out($head);
-        $(
-            print!(" ");
-            out($tail);
-        )*
-        println!();
-    };
-}
-
-#[allow(dead_code)]
-fn vout<T: std::fmt::Display>(v: Vec<T>) {
-    println!("{}", v.iter().join(" "));
-}
-macro_rules! vout {
-    () => {
-        println!();
-    };
-    ($head:expr $(, $tail:expr)*) => {
-        vout($head);
-        $(vout($tail);)*
-    };
-}
-
-#[allow(dead_code)]
-fn wout<T: std::fmt::Display>(w: Vec<Vec<T>>) {
-    each!(v, w, {
-        vout(v);
-    });
-}
 
 #[allow(dead_code)]
 const MOD: i32 = 998244353;
@@ -111,16 +18,17 @@ const dy: [i32; 9] = [0, 0, 0, -1, 1, -1, 1, -1, 1];
 const MULTI: bool = false;
 
 pub fn main() {
-    rep!(match MULTI {
+    let t = match MULTI {
         true => {
             let mut num = String::new();
             std::io::stdin().read_line(&mut num).expect("invalid input");
             num.parse().unwrap()
         }
         _ => 1
-    }, {
+    };
+    for _ in 0..t {
         solve();
-    });
+    }
 }
 
 fn solve() {
